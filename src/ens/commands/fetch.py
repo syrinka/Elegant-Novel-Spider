4b7bd471825de353aa0e5491e101d403bec31765
syrinka <skycore@foxmail.com>
2022-03-29 17:46:10 +0800

    feat: command fetch

--- src/ens/commands/fetch.py
+++ src/ens/commands/fetch.py
@@ -2,6 +2,7 @@ import click
 
 from ens.local import Local
 from ens.remote import get_remote
+from ens.console import echo, doing, Track
 from ens.typing import *
 from ens.exceptions import *
 from ens.utils.command import *
@@ -9,11 +10,35 @@ from ens.utils.command import *
 
 @click.command('fetch')
 @arg_code
-def main(code: Code):
+@click.option('-m', '--mode',
+    type = click.Choice(['update', 'flush', 'diff']),
+    default = 'update',
+    help = '''\b
+    处理数据的方式
+    - update 只抓取缺失章节，不改变已保存的章节 [default]
+    - flush  抓取全部章节并覆盖 !dangerous!
+    - diff   抓取全部章节，如为已保存章节，则对比差异''')
+@click.option('-i', '--interval',
+    type = click.FloatRange(min=0),
+    default = 0.2,
+    help = '抓取间隔（秒）[0.2]')
+@click.option('-r', '--retry',
+    type = click.IntRange(min=0),
+    default = 3,
+    help = '抓取单章时最大尝试次数，为 0 则持续尝试 [3]')
+@click.option('-t', '--thread', # TODO 多线程执行
+    type = click.IntRange(min=1),
+    default = 1,
+    hidden = True,
+    help = '同时执行的线程数')
+def main(code: Code, mode: str, interval: float, retry: int, thread: int):
+    """
+    抓取小说
+    """
     try:
         remote = get_remote(code.remote)(code)
     except RemoteNotFound:
-        raise # alert
+        raise
 
     try:
         local = Local(code)
@@ -21,7 +46,8 @@ def main(code: Code):
         local = Local.init(code)
 
     try:
-        r_novel = remote.get_info()
+        with doing('Getting Info'):
+            r_novel = remote.get_info()
     except FetchError:
         raise FetchError('Fail to get remote info.')
 
@@ -31,7 +57,8 @@ def main(code: Code):
     local.set_info(novel) # 更新信息
 
     try:
-        catalog = remote.get_catalog()
+        with doing('Getting catalog'):
+            catalog = remote.get_catalog()
     except FetchError:
         raise FetchError('Fail to get catalog.')
 
@@ -40,15 +67,30 @@ def main(code: Code):
     local.set_catalog(catalog)
 
     try:
-        index = remote.get_index()
+        with doing('Getting index'):
+            index = remote.get_index()
     except FetchError:
         raise FetchError('Fail to get index.')
 
     local.set_index(index)
 
-    cids = [cid for cid in local.spine() if (cid not in local)]
-    for cid in cids:
-        remote.get_content(cid)
+    if mode == 'update':
+        # 如为 update 模式，则只抓取缺失章节
+        cids = [cid for cid in local.spine() if (cid not in local)]
+    else:
+        cids = local.spine()
+
+    track = Track(cids, 'Fetching')
+    for cid in track:
+        content = remote.get_content(cid)
         
+        if mode == 'update':
+            local.set_chap(cid, content)
 
+        elif mode == 'flush':
+            local.set_chap(cid, content)
 
+        elif mode == 'diff':
+            raise NotImplemented('Not support for now!') # TODO
+
+    echo('Done.', style='good')
