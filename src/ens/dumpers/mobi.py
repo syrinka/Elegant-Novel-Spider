6b0a102b5f9ac542ffef85e784029359220d3cc4
syrinka <skycore@foxmail.com>
2022-04-19 12:23:43 +0800

    dumper mobi

--- src/ens/dumpers/mobi.py
+++ src/ens/dumpers/mobi.py
@@ -1,6 +1,33 @@
+import os
+
 from ens.console import echo, doing, run
 from ens.dumper import Dumper
+from ens.dumpers.epub import EPUBDumper
 
 
 class MOBIDumper(Dumper):
-    pass
+    name = 'mobi'
+    ext = '.mobi'
+
+    def init(self, meta) -> None:
+        self.path = meta.path
+        self.tmp_path = self.path + '.epub'
+        meta.path = self.tmp_path
+
+        self.epub = EPUBDumper()
+        self.epub.init(meta)
+
+    
+    def feed(self, type, data):
+        self.epub.feed(type, data)
+
+
+    def dump(self):
+        self.epub.dump()
+        run('kindlegen.exe', self.tmp_path, '-o', os.path.basename(self.path))
+        os.remove(self.tmp_path)
+
+
+    def abort(self):
+        self.epub.abort()
+        os.remove(self.tmp_path)
