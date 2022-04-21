2f9798c38ef68b986def892bc806b508ea50d302
syrinka <skycore@foxmail.com>
2022-04-21 22:39:21 +0800

    ens web debug option

--- src/ens/commands/web.py
+++ src/ens/commands/web.py
@@ -2,5 +2,7 @@ import click
 from ens.api import api
 
 @click.command('web')
-def main():
-    api.run('127.0.0.1', port=8000, debug=True)
+@click.option('-d', '--debug',
+    is_flag = True)
+def main(debug):
+    api.run('127.0.0.1', port=8000, debug=debug)
