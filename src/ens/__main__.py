6b877644d7fd8b79fa3c5c0f0c1904c7631012e3
syrinka <skycore@foxmail.com>
2022-05-23 22:07:11 +0800

    perf: 入口点异常处理

--- src/ens/__main__.py
+++ src/ens/__main__.py
@@ -1,3 +1,4 @@
+from ens.config import DEBUG
 from ens.console import console
 from ens.exceptions import ENSError
 
@@ -8,6 +9,9 @@ if __name__ == '__main__':
     try:
         ens_cli()
     except ENSError as e:
-        console.print(e)
+        if DEBUG:
+            console.print_exception()
+        else:
+            console.print(e)
     except Exception as e:
         console.print_exception()
