2c2f2dd9d1087e4d1e11b31db9dfc3fc0cf2f1fa
syrinka <syrinka@foxmail.com>
2022-08-08 21:33:56 +0800

    修改 paths

--- ens/paths.py
+++ ens/paths.py
@@ -3,13 +3,10 @@ from os.path import join, dirname
 
 # 工作目录
 CWD = dirname(dirname(__file__))
-
+STATUS = join(CWD, '.ens.status')
 LOCAL = join(CWD, 'local')
 DUMP = join(CWD, 'dump')
 
 RES = join(CWD, 'ens', 'resources')
-
-STATUS = join(RES, 'status.yml')
 MANUAL = join(RES, 'man')
-
 FLASK_PATH = join(RES, 'flask')
