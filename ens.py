833f62dca9ec231a3252b576a8bb8e76b88685ac
syrinka <syrinka@foxmail.com>
2022-08-08 22:53:35 +0800

    修改入口点

---
+++ ens.py
@@ -0,0 +1,3 @@
+#/bin/env python3
+from ens.__main__ import run
+run()
--- ens/__main__.py
+++ ens/__main__.py
@@ -1,11 +1,10 @@
 from ens.config import DEBUG
 from ens.console import console
 from ens.exceptions import ENSError
+from ens.cli import ens_cli
 
-if __name__ == '__main__':
 
-    from ens.cli import ens_cli
-    
+def run():
     try:
         ens_cli()
     except ENSError as e:
@@ -15,3 +14,7 @@ if __name__ == '__main__':
             console.print(e)
     except Exception as e:
         console.print_exception()
+
+
+if __name__ == '__main__':
+    run()
