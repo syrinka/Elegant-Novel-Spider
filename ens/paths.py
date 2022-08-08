075365a9d42036f5fa2293d6e68902312550237c
syrinka <syrinka@foxmail.com>
2022-08-08 15:36:18 +0800

    fix: 路径配置

--- ens/paths.py
+++ ens/paths.py
@@ -1,25 +1,15 @@
-import os
 from os.path import join, dirname
 
 
-def dn(path, layer=1):
-    while layer:
-        path = dirname(path)
-        layer -= 1
-    return path
-
-
-CWD = dn(__file__, 3) # 工作目录
+# 工作目录
+CWD = dirname(dirname(__file__))
 
 LOCAL = join(CWD, 'local')
 DUMP = join(CWD, 'dump')
 
-RES = join(CWD, 'src', 'resources')
+RES = join(CWD, 'ens', 'resources')
 
-APP = join(RES, 'app')
-REMOTE_RES = join(RES, 'remote-res')
 STATUS = join(RES, 'status.yml')
-TOPIC = join(RES, 'topic')
 MANUAL = join(RES, 'man')
 
 FLASK_PATH = join(RES, 'flask')
