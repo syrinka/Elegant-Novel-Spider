eae00d13c7cd9e23a1cfb35e30f09c60dca2ba04
syrinka <syrinka@foxmail.com>
2022-08-10 17:09:29 +0800

    auto open browser

--- ens/commands/web.py
+++ ens/commands/web.py
@@ -1,3 +1,4 @@
+import webbrowser
 import click
 from ens.api import api
 
@@ -5,4 +6,5 @@ from ens.api import api
 @click.option('-d', '--debug',
     is_flag = True)
 def web(debug):
+    webbrowser.open('http://127.0.0.1:8000')
     api.run('127.0.0.1', port=8000, debug=debug)
