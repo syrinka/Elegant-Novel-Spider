57b746942a3a045937c111df2a39967f36e98880
syrinka <syrinka@foxmail.com>
2023-01-26 19:07:58 +0800

    dotenv config

--- ens/config.py
+++ ens/config.py
@@ -1,10 +1,8 @@
 from typing import Optional
-
-from ens.paths import CONFIG
 from pydantic import BaseSettings
 
 
-class Config(BaseSettings):
+class ENSConfig(BaseSettings):
     LOG_LEVEL: int = 0
     CODE_DELIM: str = '~'
     CODE_INDEX_INDICATOR: str = '#'
@@ -15,7 +13,7 @@ class Config(BaseSettings):
     DO_MERGE: Optional[str]
 
     class Config(object):
-        env_file = CONFIG
+        env_file = ['.env', '.env.example']
 
 
-config = Config()
+config = ENSConfig()
